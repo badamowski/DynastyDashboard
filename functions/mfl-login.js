9c3c641b7705474a51f3fc04f66f9f1b25135c0e
Adamowski, Billy <Adamowski.Billy@principal.com>
2020-12-07 13:44:07 -0600

    debugging

--- functions/mfl-login.js
+++ functions/mfl-login.js
@@ -1,8 +1,13 @@
 const https = require('https')
 
 exports.handler = async function(event, context, callback) {
-
-  console.log("Logging in", event.body.mflUsername);
+  console.log("path", event.path);
+  console.log("httpMethod", event.httpMethod);
+  console.log("headers", event.headers);
+  console.log("queryStringParameters", event.queryStringParameters);
+  console.log("body", event.body);
+  console.log("body", event.body);
+  console.log("isBase64Encoded", event.body.isBase64Encoded);
   var options = {
     hostname: "api.myfantasyleague.com",
     path: `/2020/login?USERNAME=${event.body.mflUsername}&PASSWORD=${event.body.mflPassword}&XML=1`,
