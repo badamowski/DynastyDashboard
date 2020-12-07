280fd51aa2647447c9197b1efe0eda7f1f048cd7
Adamowski, Billy <Adamowski.Billy@principal.com>
2020-12-07 13:49:28 -0600

    validation

--- functions/mfl-login.js
+++ functions/mfl-login.js
@@ -1,40 +1,54 @@
 const https = require('https')
 
 exports.handler = async function(event, context, callback) {
-  console.log("path", event.path);
-  console.log("httpMethod", event.httpMethod);
-  console.log("headers", event.headers);
-  console.log("queryStringParameters", event.queryStringParameters);
-  console.log("body", event.body);
-  console.log("body", event.body);
-  console.log("isBase64Encoded", event.body.isBase64Encoded);
-  var options = {
-    hostname: "api.myfantasyleague.com",
-    path: `/2020/login?USERNAME=${event.body.mflUsername}&PASSWORD=${event.body.mflPassword}&XML=1`,
-    method: "POST"
-  };
+  if(event.path == "/.netlify/functions/mfl-login" && event.httpMethod == "POST"){
+    var body = JSON.parse(event.body);
 
-  const req = https.request(options, response => {
-    console.log("response", response);
-    console.log("headers", res.headers);
-
-    res.on("data", data => {
-      console.log("data", data);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
+    if(body.mflUsername && body.mflPassword){
+      console.log("Logging In", body.mflUsername);
+      var options = {
+        hostname: "api.myfantasyleague.com",
+        path: `/2020/login?USERNAME=${body.mflUsername}&PASSWORD=${body.mflPassword}&XML=1`,
+        method: "POST"
       };
-    })
-  })
 
-  req.on("error", error => {
-    console.error(error);
+      const req = https.request(options, response => {
+        console.log("response", response);
+        console.log("headers", res.headers);
 
+        res.on("data", data => {
+          console.log("data", data);
+          return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+          };
+        })
+      })
+
+      req.on("error", error => {
+        console.error(error);
+
+        return {
+          statusCode: 500,
+          body: JSON.stringify(error),
+        };
+      })
+
+      req.end()
+    }else{
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "invalid request"
+        }),
+      };
+    }
+  }else{
     return {
-      statusCode: 500,
-      body: JSON.stringify(error),
+      statusCode: 400,
+      body: JSON.stringify({
+        error: "invalid request"
+      }),
     };
-  })
-
-  req.end()
+  }
 }
