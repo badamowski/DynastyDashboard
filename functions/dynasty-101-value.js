492d616f9225a79fd3adb10914b87285e58a3f40
Adamowski, Billy <Adamowski.Billy@principal.com>
2020-12-14 09:27:09 -0600

    post with form

--- functions/dynasty-101-value.js
+++ functions/dynasty-101-value.js
@@ -7,10 +7,16 @@ exports.handler = function(event, context, callback) {
     if(body.info && body.QB){
       console.log("dynasty-101-value", body.info);
 
+      var postData = "info:" + body.info + "\nQB:" + body.QB;
+      console.log(postData);
       var options = {
         hostname: "dynasty101.com",
         path: "/calculator/loadData.php",
-        method: "POST"
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': postData.length
+        }
       };
 
       const req = https.request(options, response => {
@@ -36,6 +42,8 @@ exports.handler = function(event, context, callback) {
         	body: error
         });
       });
+
+      req.write(postData);
 
       req.end()
 
